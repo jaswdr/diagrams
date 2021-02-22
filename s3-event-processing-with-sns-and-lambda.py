ce6ea07e0ef78879af8d78273bd7808aa33bc058
Jonathan Schweder <jonathanschweder@gmail.com>
2021-02-22 09:10:36 +0000

    Added example with multiple Lambdas functions in S3 Event processing diagram

--- s3-event-processing-with-sns-and-lambda.py
+++ s3-event-processing-with-sns-and-lambda.py
@@ -4,4 +4,4 @@ from diagrams.aws.integration import SNS
 from diagrams.aws.compute import Lambda
 
 with Diagram(filename="s3-event-processing-with-sns-and-lambda", outformat="jpg"):
-    S3("S3 Bucket") >> SNS("SNS Topic") >> Lambda("Lambda Function")
+    S3("S3 Bucket") >> SNS("SNS Topic") >> [Lambda("Lambda Function 1"), Lambda('Lambda Function 2')]
