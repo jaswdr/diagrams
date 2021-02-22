c932c55f04b7573f274fee963e06f23576d9f06e
Jonathan Schweder <jonathanschweder@gmail.com>
2021-02-22 13:04:45 +0000

    Resized S3 event processing graph

--- s3-event-processing-with-sns-and-lambda.py
+++ s3-event-processing-with-sns-and-lambda.py
@@ -3,5 +3,9 @@ from diagrams.aws.storage import S3
 from diagrams.aws.integration import SNS
 from diagrams.aws.compute import Lambda
 
-with Diagram(filename="s3-event-processing-with-sns-and-lambda", outformat="jpg"):
+graph_attr = {
+        "viewport": "450,350"
+}
+
+with Diagram(filename="s3-event-processing-with-sns-and-lambda", outformat="jpg", graph_attr=graph_attr):
     S3("S3 Bucket") >> SNS("SNS Topic") >> [Lambda("Lambda Function 1"), Lambda('Lambda Function 2')]
