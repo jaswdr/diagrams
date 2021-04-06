936e9bd0512d5648f537b99f0b69be9138002bb1
Jonathan Schweder <jonathanschweder@gmail.com>
2021-04-06 21:21:09 +0100

    Update rent prive webcrawler

--- rent-price-webcrawler.py
+++ rent-price-webcrawler.py
@@ -5,8 +5,7 @@ from diagrams.aws.compute import Lambda
 from diagrams.aws.database import DDB
 from diagrams.aws.integration import Eventbridge, SQS
 from diagrams.aws.mobile import Amplify
-from diagrams.aws.network import APIGateway, Route53
-from diagrams.aws.storage import S3
+from diagrams.aws.network import APIGateway
 
 from diagrams.onprem.client import User
 from diagrams.onprem.compute import Server
@@ -17,18 +16,14 @@ graph_attr = {
 
 with Diagram(filename="rent-price-webcrawler", outformat="jpg", direction="LR", show=False):
     cron = Eventbridge("Cron(daily)")
-    bucket_raw = SQS("Raw Data")
-    bucket_static = S3("Static Data")
 
     lambda_crawler = Lambda("Crawler")
-    lambda_extractor = Lambda("Extractor")
-    lambda_aggregator = Lambda("Aggregator")
+    lambda_enrichment = Lambda("Enrichment")
     lambda_search = Lambda("Search")
 
     ddb_table = DDB("Offers")
 
     frontend = Amplify("Frontend")
-    api_dns_entrypoint = Route53("API Entrypoint")
     api_gateway_entrypoint = APIGateway("API Entrypoint")
 
     user = User("User")
@@ -36,13 +31,10 @@ with Diagram(filename="rent-price-webcrawler", outformat="jpg", direction="LR", 
 
     with Cluster("Collection and Enrichment"):
         cron >> Edge(label="Fires") >> lambda_crawler << Edge(label="Get Web Page") << webserver
-        lambda_crawler >> Edge(label="Saves Raw Data") >> bucket_raw
-        bucket_raw >> Edge(label="Send to Listeners") >> lambda_extractor >> Edge(label="Save Enrichment Data") >> ddb_table
-        ddb_table >> Edge(label="Publish to Stream") >> lambda_aggregator
-        lambda_aggregator >> Edge(label="Save Aggregated Data") >> bucket_static
+        lambda_crawler >> Edge(label="Publish to Stream") >> ddb_table
+        ddb_table - Edge(label="Publish to Stream") - lambda_enrichment
 
     with Cluster("User interaction"):
         user >> Edge(label="Access") >> frontend
-        frontend >> Edge(label="Request") >> api_dns_entrypoint >> Edge(label="Resolves") >> api_gateway_entrypoint
+        frontend >> Edge(label="Request") >> api_gateway_entrypoint
         api_gateway_entrypoint >> Edge(label="Trigger") >> lambda_search >> Edge(label="Query") >> ddb_table
-        lambda_search >> Edge(label="Get Static") >> bucket_static
